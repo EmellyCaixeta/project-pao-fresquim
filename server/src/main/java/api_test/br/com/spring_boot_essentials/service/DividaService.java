c10db4264e369b80d75d18609cf5b062dcb16dc5
Yago Ferreira Rodrigues <yagorodrigues123456@gmail.com>
2026-05-14 19:34:31 -0300

    divida

--- server/src/main/java/api_test/br/com/spring_boot_essentials/service/DividaService.java
+++ server/src/main/java/api_test/br/com/spring_boot_essentials/service/DividaService.java
@@ -2,23 +2,46 @@ package api_test.br.com.spring_boot_essentials.service;
 
 import api_test.br.com.spring_boot_essentials.model.DividaModel;
 import api_test.br.com.spring_boot_essentials.repository.DividaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class DividaService {
 
-    @Autowired
-    private DividaRepository dividaRepository;
+    private final DividaRepository dividaRepository;
+
+    public List<DividaModel> listarTodas() {
+        return dividaRepository.findAll();
+    }
+
+    public DividaModel buscarPorId(Integer id) {
+        return dividaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Dívida não encontrada com o ID: " + id));
+    }
+
+    public DividaModel salvar(DividaModel divida) {
+        return dividaRepository.save(divida);
+    }
+
+    public DividaModel atualizar(Integer id, DividaModel divida) {
+        buscarPorId(id);
+        return dividaRepository.save(divida);
+    }
+
+    public void deletar(Integer id) {
+        DividaModel divida = buscarPorId(id);
+        dividaRepository.delete(divida);
+    }
 
     public DividaModel registrarDivida(DividaModel novaDivida) {
-
         return dividaRepository.save(novaDivida);
     }
 
     public void enviarCobranca(DividaModel divida) {
-
         String email = divida.getCliente().getEmail();
-        System.out.println("Enviando cobranca de R$" + divida.getValor() + " para: " + email);
+        System.out.println("Enviando cobrança de R$" + divida.getValor() + " para: " + email);
     }
 }
